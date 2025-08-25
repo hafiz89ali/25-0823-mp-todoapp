de96fb393633ae2ff39eea0d9054336e40835d30
hafiz89ali <hafiz89.ali@gmail.com>
2025-08-26 06:02:24 +0800

    complete login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,8 +37,8 @@ async function registerUser(req, res) {
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
 
-  // after all the data are valid then insert user into database
-  // store the hashed password in the database
+  // After all the data are valid then insert user into database
+  // Store the hashed password in the database
   try {
     const resDb = await database.query(insertUserSQL, [
       username,
@@ -59,3 +59,63 @@ async function registerUser(req, res) {
     return res.status(500).json({ error: error.message });
   }
 }
+
+async function loginUser(req, res) {
+  const selectUserSQL = "SELECT * FROM users WHERE email = $1";
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Check all fields are present
+  if (!email || !password) {
+    return res.status(400).json({ error: "Invalid email." });
+  }
+
+  // Get user from database email
+  try {
+    const resDb = await database.query(selectUserSQL, [email]);
+    if (resDb.rows.length === 0) {
+      // ***Why are we not using not found error?
+      // ***For sequrity reasons, we don't want to tell the user if the email is not found
+      // ***We want to keep the error message generic and not give away any information
+      return res.status(401).json({ error: "Invalid email or password." });
+    }
+    const user = resDb.rows[0];
+    const dbPassword = user.password;
+
+    // Compare the password with the hashed password in the database
+    const isPasswordMatch = bcrypt.compareSync(password, dbPassword);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ error: "Invalid email or password." });
+    }
+
+    // Create jwt token and send it to the user.
+    // tokenData must not include password or any sensitive information
+    // This is an encoded token which can be decoded by anyone
+    const tokenData = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET);
+
+    const resData = {
+      message: "Login successful",
+      token: token,
+    };
+    return res.status(200).json(resData);
+  } catch {
+    error;
+  }
+  {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+const authController = {
+  registerUser,
+  loginUser,
+};
+
+export default authController;
